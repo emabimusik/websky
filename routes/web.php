93f093a4f2b3a67a71a3beaaeb4a80d560b19987
emabimusik <emabi2015@gmail.com>
2021-07-12 15:50:40 +0200

    jstream migration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +20,6 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+
     return view('dashboard');
 })->name('dashboard');
